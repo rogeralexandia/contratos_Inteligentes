de7181e1b821773110a5bc0fcb370caad9cf6800
wdavilav <hcker1994>
2022-03-04 19:30:50 -0500

    update core app

--- config/settings.py
+++ config/settings.py
@@ -31,22 +31,29 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # libs
-    'widget_tweaks',
-    # apps
+]
+
+LOCAL_APPS = [
     'core.login',
     'core.pos',
     'core.reports',
     'core.user',
     'core.homepage',
 ]
+
+THIRD_PARTY_APPS = [
+    'widget_tweaks',
+    'rest_framework',
+]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
